c4125af364a58ffaefb2667b7aecbaeddd12495a
Patrick Bower <bower.patrick@gmail.com>
2018-03-16 17:18:23 +0000

    Setup basic webpack

--- scripts.js
+++
@@ -1,7 +0,0 @@
-import Form from './assets/scripts/form';
-console.log(Form);
-
-// (function(){
-    // const form = new Form();
-    // form.init();
-// })(); 
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import Form from './scripts/form';
+
+(function(){
+    const form = new Form();
+    form.init();
+})(); 
